506a2beed8a9e589e218ecd4b7f09d22dee5ad8d
ICE0208 <doky3307@naver.com>
2025-03-31 02:16:50 +0900

    feat: 스크롤 내렸을 때 배경색 어둡게

--- src/components/layouts/header/index.tsx
+++ src/components/layouts/header/index.tsx
@@ -1,9 +1,18 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./styles.module.css";
+import { useScroll } from "@/hooks/useScroll";
 
 export default function Header() {
+  // 스크롤이 100px 이상 내려갔을 때 isBgDark 값이 true로 변경됨
+  // isBgDark 값이 true일 때 containerDark 클래스가 추가되어 배경색이 검은색으로 변경됨
+  const isBgDark = useScroll(100);
+
   return (
-    <div className={styles.container}>
+    <div
+      className={`${styles.container} ${isBgDark ? styles.containerDark : ""}`}
+    >
       <div className={styles.logo}>
         <Image
           src="/images/header-logo.svg"
