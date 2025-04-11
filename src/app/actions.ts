21c32c2385f2a139e39f41db52c36ce25c3b23d3
gweonyuig <gweonyuig@gmail.com>
2025-04-11 21:34:13 +0900

    feat: 소설정보 가져오는 함수 추가

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -11,3 +11,9 @@ export async function getNovelData() {
   const response = await import("@/mocks/novelpage-novel-data.json");
   return response.default;
 }
+
+export async function getNovelInfoData() {
+  // mocks json 파일에서 데이터 가져오기
+  const response = await import("@/mocks/novelinfo-novel-data.json");
+  return response.default;
+}
