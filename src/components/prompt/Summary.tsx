5b361690c5ee9485222287f3cbaf69db681622ef
gweonyuig <gweonyuig@gmail.com>
2025-04-14 22:46:24 +0900

    feat: 교열페이지 파일생성 및 기초작업

---
+++ src/components/prompt/Summary.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Summary.module.css";
+
+const Summary = () => (
+  <div className={styles.container}>
+    <div className={styles.summary}>summary</div>
+  </div>
+);
+
+export default Summary;
