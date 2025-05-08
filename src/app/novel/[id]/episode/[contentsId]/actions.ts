83a9eaab5be0a3dffd7f1a6fe378f5f394e7a137
gweonyuig <gweonyuig@gmail.com>
2025-05-08 21:19:09 +0900

    refactor: 소설 메인 페이지 db 연동

--- src/app/novel/[id]/episode/[contentsId]/actions.ts
+++
@@ -1,7 +0,0 @@
-"use server";
-
-export async function getNovelContentsData() {
-  // mocks json 파일에서 데이터 가져오기
-  const response = await import("@/mocks/novelcontents-novel-data.json");
-  return response.default;
-}
