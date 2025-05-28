e6ced9d0b2ec1cd847ec88fd7612f6f026bc0f9a
ICE0208 <doky3307@naver.com>
2025-05-29 02:46:38 +0900

    fix: 이상한 minHeight 제거

--- src/app/(full-screen)/workspaces/[id]/components/PromptInput.tsx
+++ src/app/(full-screen)/workspaces/[id]/components/PromptInput.tsx
@@ -443,7 +443,6 @@ const PromptInput = forwardRef<PromptInputHandle, PromptInputProps>(
                 style={{
                   width: "100%",
                   height: "100%",
-                  minHeight: "400px",
                   padding: "1rem",
                   border: "none",
                   borderRadius: "0",
